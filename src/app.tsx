56b904779b5285ae06b473d44048e3ead1edba7f
imanthanparmar <manthan.parmar@scaletech.xyz>
2023-06-05 11:27:32 +0530

    base project setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,35 +1,16 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
-import Login from 'features/auth/container/login';
-import ForgotPassword from 'features/auth/component/forgotPassword';
-import ResetPassword from 'features/auth/component/resetPassword';
-
-import { IState } from 'shared/interface/state';
 import Layout from 'hoc/layout/layout';
 
 const App: React.FC = () => {
-	const isLogin: boolean = useSelector((state: IState) => state.auth.isLogin);
-
-	if (isLogin) {
-		return (
-			<Layout>
-				<Routes>
-					<Route path='/' />
-				</Routes>
-			</Layout>
-		);
-	} else {
-		return (
+	return (
+		<Layout>
 			<Routes>
-				<Route path='/login' element={<Login />} />
-				<Route path='/forgot-password' element={<ForgotPassword />} />
-				<Route path='/reset-password/:token' element={<ResetPassword />} />
-				<Route path='*' element={<Navigate replace to='/login' />} />
+				<Route path='/' />
 			</Routes>
-		);
-	}
+		</Layout>
+	);
 };
 
 export default App;
