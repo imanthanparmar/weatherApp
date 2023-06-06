bc552699b106d38a2070bdeaaa1f87b343782509
imanthanparmar <manthan.parmar@scaletech.xyz>
2023-06-06 18:31:16 +0530

    resolve video issues

--- src/features/weatherForecast/container/weatherForecastContainer.tsx
+++ src/features/weatherForecast/container/weatherForecastContainer.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import Video from '../component/video';
 import '../style/weatherForecast.scss';
 import WeatherForecast from '../component/weatherForecast';
 
 const WeatherForecastContainer: React.FC = () => {
 	return (
 		<div className='position--relative weather_container'>
-			<Video />
 			<WeatherForecast />
 		</div>
 	);
